1bc1e0490fd3ee64f59c0e6facc2653d17baa2bb
micahelbronshteyn <0325Danik>
2021-01-26 08:54:01 -0800

    add more tests

--- src/test/java/com/mbronshteyn/vavr/FuncInterfaceTest.java
+++ src/test/java/com/mbronshteyn/vavr/FuncInterfaceTest.java
@@ -1,2 +1,16 @@
-package com.mbronshteyn.vavr;public class FuncInterfaceTest {
+package com.mbronshteyn.vavr;
+
+import io.vavr.Function1;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FuncInterfaceTest {
+    @Test
+    public void givenVavrFunction_whenWorks_thenCorrect() {
+        Function1<Integer, Integer> square = (num) -> num * num;
+        int result = square.apply(2);
+
+        assertEquals(4, result);
+    }
 }
