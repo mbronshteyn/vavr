bade5e50c1b18e08bfdded874231c397f38757bb
micahelbronshteyn <0325Danik>
2021-01-31 12:30:21 -0800

    Add asserts

--- src/test/java/com/mbronshteyn/vavr/EitherTest.java
+++ src/test/java/com/mbronshteyn/vavr/EitherTest.java
@@ -1,16 +1,17 @@
 package com.mbronshteyn.vavr;
 
 import io.vavr.control.Either;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class EitherTest {
     @Test
     public void testEitherOne() {
         Either<String, Integer> result = calculateStuff(-1);
-        System.out.println(result);
+        Assert.assertEquals(result.getLeft(), "The argument should be above 0");
 
         result = calculateStuff(3);
-        System.out.println(result);
+        Assert.assertEquals(result.get().intValue(), 30);
     }
 
     private Either<String, Integer> calculateStuff(Integer argument) {
