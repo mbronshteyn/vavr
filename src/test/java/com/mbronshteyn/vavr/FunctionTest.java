746c6cb24d1ae044563aa6e8d0b41f221403d21f
micahelbronshteyn <0325Danik>
2021-01-31 12:53:37 -0800

    Add asserts

--- src/test/java/com/mbronshteyn/vavr/FunctionTest.java
+++ src/test/java/com/mbronshteyn/vavr/FunctionTest.java
@@ -1,7 +1,6 @@
 package com.mbronshteyn.vavr;
 
-import io.vavr.Function1;
-import io.vavr.Function2;
+import io.vavr.*;
 import io.vavr.control.Option;
 import org.junit.Assert;
 import org.junit.Test;
@@ -54,6 +53,43 @@ public class FunctionTest {
         Function2<Integer, Integer, Integer> sum = (a, b) -> a + b;
         Function1<Integer, Integer> add2 = sum.apply(2);
 
-        Assert.assertEquals(add2.apply(4).intValue(), 6L);
+        Assert.assertEquals(add2.apply(4).longValue(), 6L);
+
+
+        Function5<Integer, Integer, Integer, Integer, Integer, Integer> sumFive = (a, b, c, d, e) -> a + b + c + d + e;
+        Function2<Integer, Integer, Integer> add6 = sumFive.apply(2, 3, 1);
+
+        Assert.assertEquals(add6.apply(4, 3).longValue(), 13);
+    }
+
+    @Test
+    public void testCurrying(){
+        Function2<Integer, Integer, Integer> sum = (a, b) -> a + b;
+        Function1<Integer, Integer> add2 = sum.curried().apply(2);
+
+        Assert.assertEquals(add2.apply(4).longValue(), 6);
+
+        //////////////////////////////////////
+
+        Function3<Integer, Integer, Integer, Integer> sumThree = (a, b, c) -> a + b + c;
+        final Function1<Integer, Function1<Integer, Integer>> addThree = sumThree.curried().apply(2);
+
+        Assert.assertEquals(addThree.apply(4).apply(3).longValue(), 9);
+    }
+
+    /*
+    Memoization is a form of caching. A memoized function executes only once and then returns the result from a cache.
+    The following example calculates a random number on the first invocation and returns the cached number on the second invocation.
+     */
+    @Test
+    public void testMemorization(){
+        Function0<Double> hashCache =
+                Function0.of(Math::random).memoized();
+
+        double randomValue1 = hashCache.apply();
+        double randomValue2 = hashCache.apply();
+
+        Assert.assertEquals(randomValue1, randomValue2, 0.0000001);
+
     }
 }
